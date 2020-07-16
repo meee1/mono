17e2213f040b708b85e224731ae7ffd390f9ec6c
Michael Oborne <mich146@hotmail.com>
2020-07-16 16:24:57 +0800

    custom

--- mcs/class/System.Windows.Forms/System.Windows.Forms/Hwnd.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/Hwnd.cs
@@ -29,7 +29,6 @@
 using System.Collections;
 using System.Drawing;
 using System.Drawing.Imaging;
-using SkiaSharp;
 
 // NOTE: Possible optimization:
 // Several properties calculate dimensions on the fly; instead; they can 
