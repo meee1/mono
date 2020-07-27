263d0aebff52e3d88bace5e1d8a66bad43c8abb6
Michael Oborne <mich146@hotmail.com>
2020-07-27 15:50:54 +0800

    wip

--- mcs/class/System.Windows.Forms/System.Windows.Forms/XplatUI.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms/XplatUI.cs
@@ -146,9 +146,7 @@ namespace System.Windows.Forms {
 
 		public static bool RunningOnUnix {
 			get {
-				int p = (int) Environment.OSVersion.Platform;
-				
-				return (p == 4 || p == 6 || p == 128);
+				return true;
 			}
 		}
 
